f9e9a62e20b384d56c7d5e7d3cbfbb06639d44ee
pdTrade <pd.pub.dev@gmail.com>
2024-01-29 21:33:05 +0900

    最低限のセットアップ

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <ul class="flex">
+      <li class="mr-6">
+        <NuxtLink class="text-blue-500" to="/">Home</NuxtLink>
+      </li>
+      <li class="mr-6">
+        <NuxtLink class="text-blue-500" to="/auth">Auth</NuxtLink>
+      </li>
+      <li class="mr-6">
+        <NuxtLink class="text-blue-500" to="/guest">Guest</NuxtLink>
+      </li>
+      <li class="mr-6">
+        <NuxtLink class="text-blue-500" to="/login">Login</NuxtLink>
+      </li>
+      <li class="mr-6">
+        <NuxtLink class="text-blue-500" to="/register">Register</NuxtLink>
+      </li>
+    </ul>
+    <slot />
+  </div>
+</template>
